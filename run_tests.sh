82af7d9e450641a70e8b322fbd3dc72f078b4e51
Hannah Atmer <ha237@cornell.edu>
2022-12-01 10:02:54 -0800

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+echo "2 threads, 10 elements, all distributions"
 ./psrs 2 10 0
 ./psrs 2 10 1
 ./psrs 2 10 2
-./psrs 2 10 3
-./psrs 2 10 4
 
+echo "Max threads, 1e5 elements, all distributions"
 ./psrs 32 100000 0
 ./psrs 32 100000 1
 ./psrs 32 100000 2
-./psrs 32 100000 3
-./psrs 32 100000 4
 
-./psrs 64 100000 3
+echo "Random tests"
+./psrs 64 100000 0
 ./psrs 8 1000 2
 ./psrs 128 100000 1
 ./psrs 8 8000 1
 ./psrs 3 8000 2
-./psrs 16 100000 3
-./psrs 64 10001 4
+./psrs 16 100000 0
+./psrs 64 10001 2
 
+echo "1e7 uniformly distributed elements: 1 vs 8 threads"
+./psrs 1 10000000 0
+./psrs 8 10000000 0
